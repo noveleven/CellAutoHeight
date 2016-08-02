21dafb08aa5c5d96d4bb662d51d6598c09b7c924
noveleven <ljy_1269@163.com>
2016-08-02 23:28:36 +0800

    Initial Commit

---
+++ CellAutoHeight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CellAutoHeight
+//
+//  Created by lijinyang on 16/8/2.
+//  Copyright © 2016年 noveleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CellAutoHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellAutoHeight
+//
+//  Created by lijinyang on 16/8/2.
+//  Copyright © 2016年 noveleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
